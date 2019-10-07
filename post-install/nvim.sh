747b011d23597ccb8917869f8db38ff20b905f06
Cem Keylan <cem@ckyln.com>
2019-10-07 15:35:13 +0300

    fix curl command

--- post-install/nvim.sh
+++ post-install/nvim.sh
@@ -26,7 +26,7 @@ die() { error "$@"; exit 1; }
 
 nvim -v >/dev/null 2>&1 || die "nvim does not seem to be installed, stopping script"
 out "Installing nvim plugins"
-curl -fLO $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || die 'Could not download plug.vim'
+curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || die 'Could not download plug.vim'
 [ -e $HOME/.config/nvim/init.vim ] || die "init.vim does not exist in home directory"
 nvim -c "PlugInstall | :q | :q"
 msg "nvim post-installation complete"
